54db7b198ce2934ac7d9d6739bc8bdcf844b2c6a
tobecoder <tobecoder@outlook.com>
2018-04-10 09:31:02 -0700

    [crazyJava]chapter3 Variables and operators

---
+++ 3.1/javadocTest.java
@@ -0,0 +1,23 @@
+/***
+ * Description:
+ * <br>网站
+ * <br>this progtam is protected by copyright laws.
+ * @author tobecoder tobecoder@outlook.com
+ * @version 1.0
+ */
+
+ /**
+  * javadocTest
+  */
+ public class javadocTest {
+    /**
+     *simply test 
+     */
+     protected String name;
+     /**
+      * 主方法
+      */
+      public static void main(String[] args){
+          System.out.println("hello world!");
+      }
+ }
