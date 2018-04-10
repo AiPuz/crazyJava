3d467887121be18b882ede0e6919e98e36590be1
tobecoder <tobecoder@outlook.com>
2018-04-10 09:31:02 -0700

    [crazyJava]chapter3 Variables and operators

---
+++ 3.1/Test.java
@@ -0,0 +1,21 @@
+/**
+ * Description:
+ * <br>网站
+ * <br>this is protected by copyright
+ * @author tobecoder@outlook.com
+ * @version 1.0
+ */
+
+ public class Test{
+     /**
+      * simple test
+      */
+      public int age;
+      /**
+       * test 构造
+       */
+      public Test()
+      {
+          
+      }
+ }
--- helloworld.java
+++ helloworld.java
@@ -4,8 +4,23 @@
 public class helloworld{
     public static void main(String[] args) {
         System.out.println("hello world!");
+        Person lee = new Person();
+        lee.name = "bruce";
+        lee.say(lee.name);
+        lee.jump();
     }
-    
 }
 
-#javac -d destdir srcFile
+class Person{
+    String name;
+    int age;
+    void say(String content){
+        System.out.println(content);
+    }
+
+    void jump(){
+        this.say("hello");
+    }
+}
+
+//javac -d destdir srcFile
